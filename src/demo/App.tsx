e78b97f6399529b6d7209d43389bef1fb3eef60f
Nativ-Sibony <nativ.sibony@unity3d.com>
2023-11-30 11:34:21 +0200

    .

--- src/demo/App.tsx
+++ src/demo/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useCallback, useEffect, useState } from 'react'
 
 import BubbleShooter from '../assets/images/bubble-shooter.png'
 import CandyCrash from '../assets/images/candy-crash.png'
@@ -10,7 +10,10 @@ import { type CardEvent, type CardData } from '../lib/types/types'
 
 const Content = () => (
   <div className="flex items-center justify-center">
-    <p className="text-lg">Lorem ipsum dolor sit amet.</p>
+    <p className="text-lg p-3">
+      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eum sapiente in obcaecati quos maxime, sunt corporis aut
+      eius accusamus placeat numquam fugiat. Voluptatem, vero iste.
+    </p>
   </div>
 )
 
@@ -42,21 +45,64 @@ const mockData: CardData[] = [
 ]
 
 export default function App() {
+  const [withActionButtons, setWithActionButtons] = useState(false)
+  const [withEventStream, setWithEventStream] = useState(true)
+  const [defaultActionButtons, setDefaultActionButtons] = useState(false)
+  const [leftActionButton, setLeftActionButton] = useState<React.JSX.Element | undefined>()
+  const [rightActionButton, setRightActionButton] = useState<React.JSX.Element | undefined>()
+  const [events, setEvents] = useState<string[]>([])
+
+  useEffect(() => {
+    if (defaultActionButtons) {
+      setLeftActionButton(<div>Left</div>)
+      setRightActionButton(<div>Right</div>)
+    } else {
+      setLeftActionButton(undefined)
+      setRightActionButton(undefined)
+    }
+  }, [defaultActionButtons])
+
   const handleSwipe: CardEvent = (el, meta, id, action) => {
-    console.log({ el, meta, id, action })
+    setEvents((prev) => [...prev, `- ID: ${id}, Action: ${action}, Callback: ${JSON.stringify(meta)}}`])
   }
 
   return (
-    <main className="flex flex-col h-full w-full p-5">
-      <section className="flex h-full w-full relative">
-        <CardSwiper
-          data={mockData}
-          onFinish={(status) => console.log(status)}
-          onDismiss={handleSwipe}
-          withActionButtons
-          dislikeButton={<div>Left</div>}
-          likeButton={<div>Right</div>}
-        />
+    <main className="flex h-full w-full p-5">
+      <aside className="p-8 w-1/3">
+        <div className="flex items-center  gap-2 capitalize">
+          <input type="checkbox" name="with action buttons" onClick={() => setWithActionButtons((prev) => !prev)} />
+          <label htmlFor="checkbox">With action buttons</label>
+        </div>
+        <div className="flex items-center gap-2 capitalize">
+          <input type="checkbox" name="with action buttons" onClick={() => setDefaultActionButtons((prev) => !prev)} />
+          <label htmlFor="checkbox">Default action buttons</label>
+        </div>
+        <div>
+          <div className="flex items-center gap-2 capitalize">
+            <input
+              type="checkbox"
+              name="with action buttons"
+              checked={withEventStream}
+              onClick={() => setWithEventStream((prev) => !prev)}
+            />
+            <label htmlFor="checkbox">Event Stream</label>
+          </div>
+          <section className="flex flex-col text-left w-full h-[500px] relative p-5 shadow-[0_0_15px_0_rgba(0,0,0,0.2)_inset]">
+            {withEventStream && events.map((event, index) => <div key={index}>{event}</div>)}
+          </section>
+        </div>
+      </aside>
+      <section className="flex items-center justify-center h-full w-2/3 relative overflow-hidden shadow-[0_0_15px_0_rgba(0,0,0,0.2)_inset]">
+        <div className="w-[450px] p-6 h-full">
+          <CardSwiper
+            data={mockData}
+            onFinish={(status) => console.log(status)}
+            onDismiss={handleSwipe}
+            withActionButtons={withActionButtons}
+            dislikeButton={leftActionButton}
+            likeButton={rightActionButton}
+          />
+        </div>
       </section>
     </main>
   )
